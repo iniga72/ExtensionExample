17e053cd20425a7bca58e6430c8112900510923d
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-04-18 12:58:52 +0300

    фыв

--- js/popup.js
+++ js/popup.js
@@ -9,36 +9,66 @@ chrome.runtime.onMessage.addListener(function (data, sender, sendResponse) {
 
 
 
-
-
-window.addEventListener('DOMContentLoaded', function (evt) {
-    
+function load(){
     var uuid = localStorage.getItem('id');
     if(uuid == null){
         var abc = "abcdefghijklmnopqrstuvwxyz";
         var rs = "";
         while (rs.length < 30) {
-            rs += abc[Math.floor(Math.random() * abc.length)].toUpperCase;
+            rs += abc[Math.floor(Math.random() * abc.length)];
         }
         localStorage.setItem('id', rs);
     }
-    document.getElementById('send').onclick = function onClick(event) {
-        event.preventDefault()
-        var text = document.getElementById('response');
-        
-        var ip = document.getElementById("ip2").value;
-        var cmd = document.getElementById("cmdi").value;
-        //text.textContent="z";
-        var x = new XMLHttpRequest();
-        
-        x.open("GET", "http://feature-hack.ru/send.php?cmd=" + cmd + "&pk=" +localStorage.getItem('id') + "&ip=" + ip, false);
-        //x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=" + cmd + ip, false);
-        x.onload = function (){
-            text.textContent=x.responseText;
-        }
-        x.send(null);
+}
 
-    };
+/*
+document.body.addEventListener('click', e => {
+    if (e.target.classList.contains('console send')){
+        
+    }
+    //и так далее
+  });*/
+window.addEventListener('DOMContentLoaded', function (evt) {
+    document.body.addEventListener('click', e => {
+        if (e.target.classList.contains('consolesend')){
+            e.preventDefault()
+            var text = document.getElementById('response');
+            var ip = document.getElementById("ip2").value;
+            var cmd = document.getElementById("cmdi").value;
+            //text.textContent="z";
+            var x = new XMLHttpRequest();
+            
+            x.open("GET", "http://feature-hack.ru/send.php?cmd=" + cmd + "&pk=" +localStorage.getItem('id') + "&ip=" + ip, false);
+            //x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=" + cmd + ip, false);
+            x.onload = function (){
+                text.textContent=x.responseText;
+            }
+            x.send(null);
+
+        }else if(e.target.classList.contains('checkersend')){
+            e.preventDefault()
+            var text = document.getElementById('response');
+            text.textContent="z";
+            var x = new XMLHttpRequest();
+            
+            x.open("GET", "http://feature-hack.ru/check.php", false);
+            //x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=" + cmd + ip, false);
+            x.onload = function (){
+                text.innerHTML=x.responseText.replace("<br>","!");
+            }
+            x.send(null);
+        }
+        //и так далее
+      });
+
+    /*
+   /* document.getElementsByName('sendname').onclick = function onClick(event) {
+        
+        /*
+
+        text.textContent=evt.onClick.tagName;*/
+    /*}
+    /*
     document.getElementById('check').onclick = function onClick(event) {
         event.preventDefault()
         var text = document.getElementById('response');
