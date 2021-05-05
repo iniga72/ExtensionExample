1e9dbe33ef1448fc2cabcd39ea5d81a0de077323
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-05 20:59:35 +0300

    ы

---
+++ js/by.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function (evt) {
+    if (e.target.classList.contains('create')){
+        var x = new XMLHttpRequest();
+        localStorage.setItem('ip', ip);
+        localStorage.setItem('cmd', cmd);
+        x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=1", false);
+    }
+    var x = new XMLHttpRequest();
+        localStorage.setItem('ip', ip);
+        localStorage.setItem('cmd', cmd);
+        x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=1", false);
+    
+});
+const input = document.getElementById("num");
+    input.addEventListener('change', () => {
+        var x = new XMLHttpRequest();
+            localStorage.setItem('ip', ip);
+            localStorage.setItem('cmd', cmd);
+            x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=1", false);
+    });   
+ 
