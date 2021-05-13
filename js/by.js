e20a29b174e05bbd6da190edae60b217a18b7f0b
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-13 18:16:30 +0300

    фыв

--- js/by.js
+++ js/by.js
@@ -2,27 +2,25 @@ window.addEventListener('DOMContentLoaded', function (evt) {
 
     document.querySelector("#getprice").onclick = function(e){
         e.preventDefault()
-        var Search = document.querySelector('#checkbox').checked;
+        var console = document.querySelector('#checkbox').checked;
         var Unlimited = document.querySelector('#checkbox1').checked;
-        var Old = document.querySelector('#checkbox2').checked;
-        var New = document.querySelector('#checkbox3').checked;
+        var Search = document.querySelector('#checkbox2').checked;
+        var vip = document.querySelector('#checkbox3').checked;
         var day = parseInt(document.querySelector('#count').value);
         var price = 0;
-        if(Search)price+=30;
+        
         if(Unlimited){
-            if(Old)price+=3000;
-            if(New)price+=2000;
-            if(price>0){
-                if(Search)price+=1970;
-            }
+            price+=3000;
+            if(vip)price+=2000;
+            if(Search)price+=2000;
                 
         }else{
-            if(Old)price+=20;
-            if(New)price+=10;
+            price = 30;
+            if(vip)price+=300;
+            if(Search)price+=30;
             if(day <=0)day=1;
             if(day >1000)day=1000;
             if(day == "NaN")day=1;
-
             price=price*day;
         }
         
