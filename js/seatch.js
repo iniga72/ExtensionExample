8751a154fd38929f2db98cb77afcaaa52e849d19
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-22 12:19:28 +0300

    up

--- js/seatch.js
+++ js/seatch.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', function (evt) {
         e.preventDefault()
         var text = document.getElementById('response');
         var x = new XMLHttpRequest();
-        x.open("GET", "http://exe.feature-hack.ru/search.php", false);
+        x.open("GET", "http://exe.feature-hack.ru/search.php?pk=" +localStorage.getItem('id'), false);
         //x.open("GET", "http://exe.feature-hack.ru/test/vk.php?amount=" + cmd + ip, false);
         x.onload = function (){
             text.innerHTML=x.responseText;
