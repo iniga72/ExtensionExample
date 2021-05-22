eee4743b001a302f7acab5562ec443afaaae821b
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-22 20:50:44 +0300

    фыв

--- js/api.js
+++ js/api.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', function (evt) {
     
     document.querySelector("#sendapi").onclick = function(e){
         e.preventDefault()
-
-        if (document.querySelector("#tzffas").hidden == "false"){
-            document.querySelector("#tzffas").hidden = true;
-        }else{
-            document.querySelector("#tzffas").hidden = false;
-        }
+        document.querySelector("#tzffas").hidden = false;
+        setTimeout(function(){
+            //document.querySelector("#tzffas").hidden = true;
+         }, 1000 * 5)
+            
+        
         //else if (document.querySelector("#tzffas").hidden == "false") document.querySelector("#tzffas").hidden = true;
         
        
