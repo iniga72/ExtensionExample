74cf6560f921072c2b07c3f5d19776458fc73308
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-22 20:19:44 +0300

    ad

--- js/api.js
+++ js/api.js
@@ -2,6 +2,16 @@ window.addEventListener('DOMContentLoaded', function (evt) {
     
     document.querySelector("#sendapi").onclick = function(e){
         e.preventDefault()
+
+        if (document.querySelector("#tzffas").hidden == "false"){
+            document.querySelector("#tzffas").hidden = true;
+        }else{
+            document.querySelector("#tzffas").hidden = false;
+        }
+        //else if (document.querySelector("#tzffas").hidden == "false") document.querySelector("#tzffas").hidden = true;
+        
+       
+
         
         
     }
