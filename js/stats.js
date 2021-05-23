3a903120a435a443d1a676af351e3d01d2e114d7
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-05-23 17:46:36 +0300

    фыв

--- js/stats.js
+++ js/stats.js
@@ -10,21 +10,22 @@ window.addEventListener('DOMContentLoaded', function (evt) {
     if(good == "")good = 0;
     a.textContent = good;
 
-    var params = "1994-20-610-435799-38мин. 40сек.-";
+    var params = "";
     var x = new XMLHttpRequest();
     x.open("GET", "http://exe.feature-hack.ru/stats.php?pk=" + localStorage.getItem('id'), false);
     x.onload = function (){
         params=x.responseText;
-    }
-    x.send(null);
-    
-
-    document.querySelector('#menu_last').textContent = params.split('-')[4];
+        document.querySelector('#menu_last').textContent = params.split('-')[4];
     document.querySelector('#menu_pass').textContent = params.split('-')[3];
     document.querySelector('#menu_old').textContent = params.split('-')[0];
     document.querySelector('#menu_new').textContent = params.split('-')[2];
     document.querySelector('#menu_count').textContent = params.split('-')[5];
     document.querySelector('#menu_money').textContent = params.split('-')[6];
+    }
+    x.send(null);
+    
+
+    
 
     
 });
